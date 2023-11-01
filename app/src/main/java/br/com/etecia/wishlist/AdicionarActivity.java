5026d1569f9b83ba9d813bc36bcca6e4d294ebe7
GabriiellAugustto <gabriel.gauitarra.05@gmail.com>
2023-10-31 21:50:29 -0300

    Edit1

--- app/src/main/java/br/com/etecia/wishlist/AdicionarActivity.java
+++ app/src/main/java/br/com/etecia/wishlist/AdicionarActivity.java
@@ -2,56 +2,120 @@ package br.com.etecia.wishlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class AdicionarActivity extends AppCompatActivity {
 
-    EditText editText,editText1;
+    private static final int CODE_POST_REQUEST = 1025;
+    private static final int CODE_GET_REQUEST = 1024;
+
+    EditText editText, editText1;
     Button button;
     boolean isUpdating = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.adicionar_layout);
 
-        editText=findViewById(R.id.edidescricao);
-        editText1=findViewById(R.id.edidata);
-        button=findViewById(R.id.buttonAdd);
+        editText = findViewById(R.id.edidescricao);
+        editText1 = findViewById(R.id.edidata);
+        button = findViewById(R.id.buttonAdd);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isUpdating){updatelist();}
-                else {createlist}
-
+                if (isUpdating) {
+                    updatelist();
+                } else {
+                    createlist();
+                }
             }
         });
-        
-        public void createlist(){
-            String descriçao = editText.getText().toString().trim();
-            String data = editText1.getText().toString().trim();
+        readlist();
+    }
 
-            if (TextUtils.isEmpty(descriçao)){
-                editText.setError("Erro");
-                editText.requestFocus();
-                return;
-            }
-            if (TextUtils.isEmpty(descriçao)){
-                editText1.setError("Erro");
-                editText1.requestFocus();
-                return;
-            }
+    private void createlist() {
+        String descricao = editText.getText().toString().trim();
+        String data = editText1.getText().toString().trim();
 
-            a
+        if (TextUtils.isEmpty(descricao)) {
+            editText.setError("erro");
+            editText.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(data)) {
+            editText1.setError("erro");
+            editText1.requestFocus();
+            return;
+        }
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("descricao ", descricao);
+        params.put("data", data);
 
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_CREATE_LIST, params, CODE_POST_REQUEST);
+        request.execute();
+    }
 
+    private void readlist() {
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_READ_LIST, null, CODE_GET_REQUEST);
+        request.execute();
+    }
 
+    private void updatelist() {
+        String descricao = editText.getText().toString().trim();
+        String data = editText1.getText().toString().trim();
 
+        if (TextUtils.isEmpty(descricao)) {
+            editText.setError("error");
+            editText.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(data)) {
+            editText1.setError("error");
+            editText1.requestFocus();
+            return;
+        }
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("descricao", descricao);
+        params.put("data", data);
+
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_UPDATE_LIST, params, CODE_POST_REQUEST);
+
+        button.setText("Adicionar");
+
+        editText.setText("");
+        editText1.setText("");
+
+        isUpdating = false;
+
+    }
+
+    private void delete(int id) {
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_DELETE_LIST + id, null, CODE_GET_REQUEST);
+        request.execute();
+    }
+
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String> {
+
+        public PerformNetworkRequest(String urlUpdateList, HashMap<String, String> params, int codePostRequest) {
+
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return null;
         }
     }
 }
--- app/src/main/java/br/com/etecia/wishlist/Api.java
+++ app/src/main/java/br/com/etecia/wishlist/Api.java
@@ -4,8 +4,8 @@ public class Api {
 
         private static final String ROOT_URL = "http://ipconfig/HeroApi/v1/Api.php?apicall=";
 
-        public static final String URL_CREATE_HERO = ROOT_URL + "createlist";
-        public static final String URL_READ_HEROES = ROOT_URL + "getlist";
-        public static final String URL_UPDATE_HERO = ROOT_URL + "updatelist";
-        public static final String URL_DELETE_HERO = ROOT_URL + "deletelist&id=";
+        public static final String URL_CREATE_LIST = ROOT_URL + "createlist";
+        public static final String URL_READ_LIST = ROOT_URL + "getlist";
+        public static final String URL_UPDATE_LIST = ROOT_URL + "updatelist";
+        public static final String URL_DELETE_LIST = ROOT_URL + "deletelist&id=";
 }
